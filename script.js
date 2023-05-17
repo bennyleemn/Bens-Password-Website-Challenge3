6f82e72bbe70f10dc4fdc7bedb62eac890811371
Ben Currier <benjamin.currier1@gmail.com>
2023-05-17 17:10:00 -0500

    new changes wed

--- script.js
+++ script.js
@@ -16,8 +16,8 @@
 // *****************************************************
 
 // Goals: 
-// Use above starter code to:
-    // Confirm whether to include uppercase, lowercase, symbols or numbers.
+// Use above starter code to (uncomment to use it):
+    // UI Confirm whether to include uppercase, lowercase, symbols or numbers.
         // Declare what are acceptable variables (alpha, num, sym)
     // Validate for all inputs
     // Generate Password
@@ -40,50 +40,62 @@ function_aPasswordGenerator = function(variable) {
     console.log("Enter something")
     return
 }
-    aPasswordGenerator = {
-    
-        bUpperCase: false,
-        bLowerCase: false,
-        bSymbols: false,
-        bNumeric: false,
-        bHasUpper: false,
-        bHasLower: false,
-        bHasSymbols: false,
-        bHasNumbers: false,
-        passwordLength: 8,
-        letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 'q", "r", "s", "u", "v", "w", "x", "y", "z"],
-        numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-        symbols: ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ",", ">", ".", "?", "/", "\\"],
-        possibleCharacters: "",
+aPasswordGenerator = {
+
+    bUpperCase: false,
+    bLowerCase: false,
+    bSymbols: false,
+    bNumeric: false,
+    bHasUpper: false,
+    bHasLower: false,
+    bHasSymbols: false,
+    bHasNumbers: false,
+    passwordLength: 7,
+    passwordLengthInvalid: true,
+    passwordPossible: false,
+    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"],
+    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    symbols: ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ",", ">", ".", "?", "/", "\\"],
+    possibleCharacters: [""],
 
     writePassword: function() {
-        // Add a while loop here to ensure a number is between 8 and 128
-        this.passwordLength = prompt("How many characters will your password be? Choose a value from 8 to 128")
-            if ((passwordLength <= 128) && (passwordLength >=8))
-        // Needs to handle edge of under 8 characters and over 128
-        // Add a while loop here to retake asnwers if not possible
-        this.bUpperCase = confirm("Do you want uppercase letters?")
-        this.bLowerCase = confirm("Do you want lower case letters?")
-        this.bNumeric = confirm("Do you want numbers?")
-        this.bSymbols = confirm("Do you want symbols?")
-        // One character type MUST BE SELECTED
-        if ((this.bUpperCase) || (this.bLowerCase) || (this.bNumeric) || (this.bSymbols)){
-        console.log("One is true!")
+    
+        while (this.passwordLengthInvalid)  {  
+            this.passwordLength = prompt("How many characters will your password be? Choose a value from 8 to 128")
+            
+            if ((this.passwordLength <= 128) && (this.passwordLength >= 8)){
+                this.passwordLengthInvalid = false
+                break
+            } 
+            alert("Password length must be greater than 7 characters and less than 129 characters!")
         }
-        if (this.bUpperCase || this.bLowerCase) {
-            this.possibleCharacters += this.letters
+        
+        while (!this.passwordPossible) {
+            
+            this.bUpperCase = confirm("Do you want uppercase letters?")
+            this.bLowerCase = confirm("Do you want lower case letters?")
+            this.bNumeric = confirm("Do you want numbers?")
+            this.bSymbols = confirm("Do you want symbols?")
+
+            if (this.bUpperCase || this.bLowerCase) {
+                this.possibleCharacters += this.letters
+            }
+            if (this.bSymbols) {
+                this.possibleCharacters += this.symbols
+            }
+            if (this.bNumeric) {
+                this.possibleCharacters += this.numbers
+            }
+            if ((this.bUpperCase) || (this.bLowerCase) || (this.bNumeric) || (this.bSymbols)){
+                this.passwordPossible = true
+                break
+            }
+            alert("Must Contain at least 1 character selection!")
+            
         }
-        if (this.bSymbols) {
-            this.possibleCharacters += this.symbols
-        }
-        if (this.bNumeric) {
-            this.possibleCharacters += this.numbers
-        }
-        this.possibleCharacters.replace(",", "")
         console.log(this.possibleCharacters)
     }
         
-    
 
 }
 
