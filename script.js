c031437f799595799317aed331f9a0c57e7a79c9
Ben Currier <benjamin.currier1@gmail.com>
2023-05-18 18:29:40 -0500

    css update

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
     // Object Name
     aPasswordGenerator = {
 
@@ -108,4 +93,5 @@
         }     
     };
     
+    writePassword();
 
